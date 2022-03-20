a7f60e33b184cc48e80d6bf116a194bca105d38a
mrinal203 <msauraj9@gmail.com>
2022-03-20 15:34:16 +0530

    add

---
+++ module_2/intro.js
@@ -0,0 +1,24 @@
+// how to print in java script 
+05/03/022
+console.log(" hello world");
+
+// for terminal output --  node intro.js
+let a =10;
+console.log(10);
+
+let char = 'M';
+console.log(char);
+
+let str = "i am mrinal "
+console.log(str);
+
+let bool = true;
+console.log(bool);
+
+// output-----
+// PS D:\DEV_project\module_2> node intro.js
+//  hello world
+// 10
+// M
+// i am mrinal
+// true
